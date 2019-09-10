6e5983f2101c2388bfb270dd4c97ea36d5c62af6
Derek Groen <djgroennl@gmail.com>
2019-09-10 15:41:05 +0100

    Updated basic documentation in README.

--- FabMUSCLE.py
+++ FabMUSCLE.py
@@ -14,6 +14,9 @@ add_local_paths("FabMUSCLE")
 
 @task
 def install_muscle(config, **args):
+    """
+    Install MUSCLE3 on any machine.
+    """
     update_environment(args)
     with_config("install_muscle")
     execute(put_configs,config)
@@ -22,6 +25,9 @@ def install_muscle(config, **args):
 
 @task
 def muscle_unified(config, muscle_script="main.py", **args):
+    """
+    Run a unified MUSCLE3 script.
+    """
     update_environment(args)
     update_environment({"muscle_script":muscle_script})
     with_config(config)
@@ -29,7 +35,6 @@ def muscle_unified(config, muscle_script="main.py", **args):
     job(dict(script='muscle_unified', wall_time='0:15:0', memory='2G'),args)
 
 
-@task
 def make_muscle_ensemble(config, ymmsl_name, **args):
     """
     Creates a concurrently running MUSCLE ensemble application using a 
@@ -70,6 +75,9 @@ def run_muscle_dist(config, ymmsl_name="main.ymmsl", **args):
 
 @task
 def run_muscle_dist_local(config, ymmsl_name="main.ymmsl", **args):
+    """
+    Run a distributed MUSCLE3 example locally.
+    """
 
     scripts = ["muscle_manager {}".format(ymmsl_name)]
 
@@ -92,54 +100,3 @@ def run_muscle_dist_local(config, ymmsl_name="main.ymmsl", **args):
     local(exec_string)
 
 
-### Old FabDummy code
-
-@task
-def dummy(config,**args):
-    """Submit a Dummy job to the remote queue.
-    The job results will be stored with a name pattern as defined in the environment,
-    e.g. cylinder-abcd1234-legion-256
-    config : config directory to use to define input files, e.g. config=cylinder
-    Keyword arguments:
-            cores : number of compute cores to request
-            images : number of images to take
-            steering : steering session i.d.
-            wall_time : wall-time job limit
-            memory : memory per node
-    """
-    update_environment(args)
-    with_config(config)
-    execute(put_configs,config)
-    job(dict(script='dummy', wall_time='0:15:0', memory='2G'),args)
-
-@task
-def dummy_ensemble(config="dummy_test",**args):
-    """
-    Submits an ensemble of dummy jobs.
-    One job is run for each file in <config_file_directory>/dummy_test/SWEEP.
-    """
-    
-    path_to_config = find_config_file_path(config)
-    print("local config file path at: %s" % path_to_config)
-    sweep_dir = path_to_config + "/SWEEP"
-    env.script = 'dummy'
-    env.input_name_in_config = 'dummy.txt'
-
-    run_ensemble(config, sweep_dir, **args)
-    
-@task
-def lammps_dummy(config,**args):
-    """Submit a LAMMPS job to the remote queue.
-    The job results will be stored with a name pattern as defined in the environment,
-    e.g. cylinder-abcd1234-legion-256
-    config : config directory to use to define geometry, e.g. config=lamps_lj_liquid
-    Keyword arguments:
-            cores : number of compute cores to request
-            images : number of images to take
-            steering : steering session i.d.
-            wall_time : wall-time job limit
-            memory : memory per node
-    """
-    with_config(config)
-    execute(put_configs,config)
-    job(dict(script='lammps', wall_time = '0:15:0', lammps_input = "in.CG.lammps"),args)
--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # FabMUSCLE
-This is a dummy example plugin for FabSim3. It is meant to showcase a minimal implementation for a FabSim3 plugin.
+
+This is a preliminary launcher for the MUSCLE3 toolkit. It serves to
+automatically set up MUSCLE3 simulations, launch the manager and its submodels,
+and keep all the associated data organized.
 
 ## Installation
 Simply type `fabsim localhost install_plugin:FabMUSCLE` anywhere inside your FabSim3 install directory.
 
+## Installing MUSCLE3
+On a local machine, just use `pip3 install muscle3`
+On a remote machine where you don't have admin rights, you can use `fabsim <machine> install_muscle:install_muscle`
+
+
 ## Testing
 1. To run a unified MUSCLE test job, type `fabsim localhost muscle_unified:muscle_rd_test,muscle_script=reaction_diffusion.py`.
 1. To run a unified MUSCLE test job replicated 5 times (all identical), type `fabsim localhost muscle_unified:muscle_rd_test,muscle_script=reaction_diffusion.py,replicas=5`.
+1. To run a distributed MUSCLE test job locally, type `fabsim localhost run_muscle_dist_local:muscle_rd_distr_test,ymmsl_name=reaction_diffusion.ymmsl`.
+
+### In progress
+We are currently working to enable:
+1. `fabsim localhost run_muscle_dist:muscle_rd_distr_test,ymmsl_name=reaction_diffusion.ymmsl` (in localized job submission mode).
+1. `fabsim eagle_vecma run_muscle_dist:muscle_rd_distr_test,ymmsl_name=reaction_diffusion.ymmsl` (with slurm job submission).
+1. `fabsim eagle_vecma run_muscle_dist:muscle_rd_distr_test,ymmsl_name=reaction_diffusion.ymmsl,PilotJob=true` (with QCG pilot job submission).
+
+
 
 ## Explanation of files
 * FabMUSCLE.py - main file containing the ```fabsim localhost dummy``` command implementation.
@@ -15,4 +32,5 @@ Simply type `fabsim localhost install_plugin:FabMUSCLE` anywhere inside your Fab
 * templates/muscle_unified - template file for running all-in-one muscle scripts on the remote machine.
 * templates/muscle_manager - template file for running the MUSCLE3 manager on the remote machine.
 * templates/muscle_model - template file for running individual MUSCLE3 model scripts on the remote machine.
+* templates/muscle_distributed - flexible template file for running any MUSCLE3 part on the remote machine.
 
